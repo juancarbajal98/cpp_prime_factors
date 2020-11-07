51fd3967c4ac74b8ba59f285a2b166bd5241936c
juancarbajal98 <juancarbajal98@gmail.com>
2020-11-07 12:48:18 -0800

    more comments

--- prime_factors_no_func/main.cpp
+++ prime_factors_no_func/main.cpp
@@ -27,8 +27,11 @@ int main() {
     while(current <= upper){
         for (int i=2; i<= current;i++){
             while(current % i == 0){
+                // reduce current
                 current /= i;
+                // append another factor to the factor string for this index
                 output[counter] += (to_string(i) +  " x ");
+                // increment the factor count for this index
                 factorCounter[counter]++;
             }
         }
@@ -39,14 +42,17 @@ int main() {
     }
     
     for (int j =0;j<=output.size();j++){
+        
         // changes text in prime factors by finding the strings of length 9 == strings with only one factor
         if (factorCounter[j] == 1){
             output[j] = "prime!";
         }
         
+        // handles dangling " x "
         if(output[j].substr(output[j].length()-1,1) == " "){
             output[j] = output[j].substr(0,output[j].length()-2);
         }
+        // print out numbers followed by their respective factor string
         cout << lower + j << " : " << output[j] << endl;
     }
     return 0;
